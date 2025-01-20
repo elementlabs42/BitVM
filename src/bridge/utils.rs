ec12a4dfd174ab28cd19957f4a8980e6b897ba85
aristotleee <148852099+aristotleee@users.noreply.github.com>
2025-01-20 18:08:02 +0800

    Fix disprove tx integration test

--- src/bridge/utils.rs
+++ src/bridge/utils.rs
@@ -14,18 +14,18 @@ pub fn num_blocks_per_network(network: Network, mainnet_num_blocks: u32) -> u32 
 }
 
 pub fn get_commit_from_assert_commit_tx(assert_commit_tx: &Transaction) -> Vec<RawWitness> {
-  let mut assert_commit_witness = Vec::new();
-  for input in assert_commit_tx.input.iter() {
-    // remove script and control block from witness
-    let witness = remove_script_and_control_block_from_witness(input.witness.to_vec());
-    assert_commit_witness.push(witness);
-  }
+    let mut assert_commit_witness = Vec::new();
+    for input in assert_commit_tx.input.iter() {
+        // remove script and control block from witness
+        let witness = remove_script_and_control_block_from_witness(input.witness.to_vec());
+        assert_commit_witness.push(witness);
+    }
 
-  assert_commit_witness
+    assert_commit_witness
 }
 
 fn remove_script_and_control_block_from_witness(mut witness: Vec<Vec<u8>>) -> Vec<Vec<u8>> {
-  witness.truncate(witness.len() - 2);
+    witness.truncate(witness.len() - 2);
 
-  witness
+    witness
 }
