0f8e17065e0d3253b2d3d47aa87cb3216f447863
aristotleee <148852099+aristotleee@users.noreply.github.com>
2025-01-23 14:20:04 +0800

    Merge 'main' into rc

---
+++ bridge/src/connectors/connector_f_2.rs
@@ -0,0 +1,34 @@
+use bitcoin::{Address, Network, PublicKey, ScriptBuf, TxIn};
+use serde::{Deserialize, Serialize};
+
+use crate::{connectors::base::*, scripts::*, transactions::base::Input};
+
+#[derive(Serialize, Deserialize, Eq, PartialEq, Clone)]
+pub struct ConnectorF2 {
+    pub network: Network,
+    pub operator_public_key: PublicKey,
+}
+
+impl ConnectorF2 {
+    pub fn new(network: Network, operator_public_key: &PublicKey) -> Self {
+        ConnectorF2 {
+            network,
+            operator_public_key: *operator_public_key,
+        }
+    }
+}
+
+impl P2wshConnector for ConnectorF2 {
+    fn generate_script(&self) -> ScriptBuf {
+        generate_pay_to_pubkey_script(&self.operator_public_key)
+    }
+
+    fn generate_address(&self) -> Address {
+        Address::p2wsh(
+            &generate_pay_to_pubkey_script(&self.operator_public_key),
+            self.network,
+        )
+    }
+
+    fn generate_tx_in(&self, input: &Input) -> TxIn { generate_default_tx_in(input) }
+}
