0921f6b9ac95d0fb049236b9f3b6e6763c53fafe
franklynot <148856869+franklynot@users.noreply.github.com>
2025-02-19 19:48:31 +0100

    Simplify in-memory cache implementation

--- bridge/src/client/memory_cache.rs
+++ bridge/src/client/memory_cache.rs
@@ -1,5 +1,4 @@
 use std::{
-    borrow::Borrow,
     collections::HashMap,
     hash::Hash,
     sync::{LazyLock, RwLock},
@@ -16,24 +15,17 @@ pub struct Cache<K: Eq + Hash, V>(HashMap<K, V>);
 impl<K, V> Cache<K, V>
 where
     K: Eq + Hash,
-    V: Clone,
 {
     fn new(capacity: usize) -> Self { Self(HashMap::with_capacity(capacity)) }
 
     pub fn push(&mut self, key: K, value: V) -> Option<V> { self.0.insert(key, value) }
 
-    pub fn get<Q: ?Sized>(&self, key: &Q) -> Option<&V>
-    where
-        K: Borrow<Q>,
-        Q: Hash + Eq,
+    pub fn get(&self, key: &K) -> Option<&V>
     {
         self.0.get(key)
     }
 
-    pub fn contains<Q: ?Sized>(&self, key: &Q) -> bool
-    where
-        K: Borrow<Q>,
-        Q: Hash + Eq,
+    pub fn contains(&self, key: &K) -> bool
     {
         self.0.contains_key(key)
     }
